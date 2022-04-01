d3bd606bcf5b5f5ed116b94d94da94f1de166b65
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-02 01:06:07 +0600

    heading component

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from 'reactstrap';
+
+const Heading = () => {
+    return (
+        <div>
+         <Navbar>
+        
+        </Navbar>  
+        </div>
+    );
+};
+
+export default Heading;
