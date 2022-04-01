da5984af94f0ba406dd18b6d3a390f212c44c8a7
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-01 23:58:05 +0600

    edit user page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import AddUser from './components/AddUser/AddUser';
+import EditUser from './components/EditUser/EditUser';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route  path='/' element={<Home/>}/>
         <Route path="adduser" element={<AddUser/>}/>
+        <Route path='edit:id' element={<EditUser/>}/>
       </Routes>
       </BrowserRouter>
     
