70269e7d7c38cc22b8a03860f72560ceced8da75
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-04 00:14:57 +0600

    value done

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -42,16 +42,19 @@ const AddUser = () => {
                     <Label>Add Name</Label>
                     <Input type ="text"  
                     name="username"
+                    value={uservalue.username}
                     onChange={handleFormValue}
                     ></Input>
                     <Label>Add Email</Label>
                     <Input type ="email" 
                     name="email"
+                    value={uservalue.email}
                     onChange={handleFormValue}
                     ></Input>
                     <Label>Add password</Label>
                     <Input type ="text"
                      name="password"
+                     value={uservalue.password}
                      onChange = {handleFormValue}
                     ></Input>
                    </FormGroup>
