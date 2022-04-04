ada8f90d1f22baab87bc53b8fdaf70cf23ddc83e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-05 00:16:14 +0600

    nothing

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 import AddUser from '../AddUser/AddUser';
 
