e13a2690f5279073ff33133014a4ab117c7fb15a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-04 23:18:46 +0600

    state value change

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, ListGroup, ListGroupItem } from 'reactstrap';
+import AddUser from '../AddUser/AddUser';
 
 const UserList = () => {
     const [uservalue,setUservalue]=useState({
@@ -8,10 +9,15 @@ const UserList = () => {
         email:'',
         password:''
     });
+    const handleFormValue = (e)=>{
+        const nameValue=e.target.name;
+        const inputValue = e.target.value;
+        setUservalue({...uservalue,[nameValue]:inputValue})
+    }
     return (
        <ListGroup className='mt-4'>
         <ListGroupItem className='d-flex '>
-            <strong></strong>
+            <AddUser handleFormValue={handleFormValue}></AddUser>
             <div className='ml-auto'>
                 <Link to ='edit/1' className='btn btn-warning mr-1'>Edit</Link>
                 <Button className='btn btn-danger'>Delete</Button>
